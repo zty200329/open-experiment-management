19a852f243e0f2d55399b4491633adf2b1a60ac0
1256233771 <1256233771@qq.com>
2018-12-17 22:03:23 +0800

    git init

---
+++ src/main/java/com/swpu/uchain/openexperiment/OpenExperimentApplication.java
@@ -0,0 +1,14 @@
+package com.swpu.uchain.openexperiment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenExperimentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenExperimentApplication.class, args);
+    }
+
+}
+
---
+++ src/main/java/com/swpu/uchain/openexperiment/model/User.java
@@ -0,0 +1,17 @@
+package com.swpu.uchain.openexperiment.model;
+
+/**
+ * @Author: clf
+ * @Date: 18-12-17
+ * @Description:
+ */
+
+public class User {
+    private String IDCard;//身份证号
+    private String fullname;//真实姓名
+    private String password;
+    private String role;
+    private String mobilePhone;//手机号
+    private String fixPhone;//固定电话
+    private String email;
+}
