9fc3dd15d46c7e823f04a598bc41557b2712aecf
hobo <1056024860@qq.com>
2019-01-25 18:19:54 +0800

    update

--- src/main/java/com/swpu/uchain/openexperiment/form/file/UploadFileForm.java
+++ src/main/java/com/swpu/uchain/openexperiment/form/file/UploadFileForm.java
@@ -1,7 +1,10 @@
 package com.swpu.uchain.openexperiment.form.file;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.constraints.NotNull;
 
 /**
  * @Description
@@ -11,8 +14,12 @@ import org.springframework.web.multipart.MultipartFile;
 @Data
 public class UploadFileForm {
 
-
+    @ApiModelProperty("要上传的文件")
+    @NotNull(message = "上传文件不能为空")
     private MultipartFile file;
 
+    @ApiModelProperty("项目组Id")
+    @NotNull(message = "项目组Id不能为空")
+    private Long projectGroupId;
 
 }
