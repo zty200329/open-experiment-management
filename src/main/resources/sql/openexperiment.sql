3b9208640d1d4b10b7960ad7a35adb24e9e3a131
louye <201631063220>
2019-06-18 23:41:21 +0800

    add sql file

--- src/main/resources/sql/openexperiment.sql
+++ src/main/resources/sql/openexperiment.sql
@@ -1,36 +1,41 @@
-/*
-Navicat MySQL Data Transfer
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4541
+#
+# http://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Host: 118.24.9.53 (MySQL 5.7.21-0ubuntu0.16.04.1)
+# Database: openexperiment
+# Generation Time: 2019-06-18 15:38:41 +0000
+# ************************************************************
 
-Source Server         : 118.24.9.53
-Source Server Version : 50725
-Source Host           : localhost:3306
-Source Database       : openexperiment
 
-Target Server Type    : MYSQL
-Target Server Version : 50725
-File Encoding         : 65001
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-Date: 2019-04-07 09:32:53
-*/
 
-SET FOREIGN_KEY_CHECKS=0;
+# Dump of table acl
+# ------------------------------------------------------------
 
--- ----------------------------
--- Table structure for acl
--- ----------------------------
-DROP TABLE IF EXISTS `acl`;
 CREATE TABLE `acl` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `description` varchar(255) DEFAULT NULL COMMENT '对权限的具体描述',
   `name` varchar(255) DEFAULT NULL COMMENT '权限名称',
   `url` varchar(255) DEFAULT NULL COMMENT '具体接口的url',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=36 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Table structure for announcement
--- ----------------------------
-DROP TABLE IF EXISTS `announcement`;
+
+
+# Dump of table announcement
+# ------------------------------------------------------------
+
 CREATE TABLE `announcement` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `publisher_id` bigint(20) DEFAULT NULL COMMENT '发布者id',
@@ -41,10 +46,11 @@ CREATE TABLE `announcement` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Table structure for funds
--- ----------------------------
-DROP TABLE IF EXISTS `funds`;
+
+
+# Dump of table funds
+# ------------------------------------------------------------
+
 CREATE TABLE `funds` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `amount` int(11) DEFAULT NULL COMMENT '资金数额',
@@ -58,10 +64,11 @@ CREATE TABLE `funds` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Table structure for notice
--- ----------------------------
-DROP TABLE IF EXISTS `notice`;
+
+
+# Dump of table notice
+# ------------------------------------------------------------
+
 CREATE TABLE `notice` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `title` varchar(255) DEFAULT NULL COMMENT '通知标题',
@@ -71,10 +78,11 @@ CREATE TABLE `notice` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Table structure for polemic_group
--- ----------------------------
-DROP TABLE IF EXISTS `polemic_group`;
+
+
+# Dump of table polemic_group
+# ------------------------------------------------------------
+
 CREATE TABLE `polemic_group` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL COMMENT '分组名称',
@@ -84,26 +92,28 @@ CREATE TABLE `polemic_group` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Table structure for project_file
--- ----------------------------
-DROP TABLE IF EXISTS `project_file`;
+
+
+# Dump of table project_file
+# ------------------------------------------------------------
+
 CREATE TABLE `project_file` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `project_group_id` bigint(20) DEFAULT NULL,
   `download_times` int(11) DEFAULT NULL COMMENT '下载次数',
   `file_name` varchar(255) DEFAULT NULL COMMENT '文件名',
-  `file_type` int(11) DEFAULT NULL COMMENT '文件类型：1.excel,2.word,3.viedo,4.image',
+  `file_type` int(11) DEFAULT NULL COMMENT '文件类型：1.excel,2.word,3.video,4.image',
   `size` varchar(255) DEFAULT NULL COMMENT '文件大小',
   `upload_time` datetime DEFAULT NULL COMMENT '上传时间',
   `upload_user_id` bigint(20) DEFAULT NULL COMMENT '上传者id',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Table structure for project_group
--- ----------------------------
-DROP TABLE IF EXISTS `project_group`;
+
+
+# Dump of table project_group
+# ------------------------------------------------------------
+
 CREATE TABLE `project_group` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `address` varchar(255) DEFAULT NULL COMMENT '实验地点',
@@ -111,7 +121,7 @@ CREATE TABLE `project_group` (
   `creator_id` bigint(20) DEFAULT NULL COMMENT '申报者id',
   `end_time` datetime DEFAULT NULL COMMENT '实验截止时间',
   `experiment_condition` varchar(800) DEFAULT NULL COMMENT '实验条件(描述)',
-  `suggest_group_type` int(11) DEFAULT NULL COMMENT '建议评分分组:1.A组石工地堪,2.B组化工材料3.C组机械力学4.D电气及制作,5.E组软件与数学,6.F组经管法律艺体人文',
+  `suggest_group_type` int(11) DEFAULT NULL COMMENT '建议评分分组:1.A组石工地堪,2.B组化工材料3.C组机械力学4.E组软件与数学,5.F组经管法律艺体人文',
   `experiment_type` int(11) DEFAULT NULL COMMENT '实验类型：1.科研，2.科技活动，3.自选课题，4.计算机应用，5.人文素质',
   `achievement_check` varchar(800) DEFAULT NULL COMMENT '成果考核方式',
   `limit_college` varchar(255) DEFAULT NULL COMMENT '限选学院',
@@ -125,13 +135,15 @@ CREATE TABLE `project_group` (
   `start_time` datetime DEFAULT NULL COMMENT '实验开始时间',
   `status` int(11) DEFAULT NULL COMMENT '实验开展进度，根据需求进行确定',
   `update_time` datetime DEFAULT NULL COMMENT '更新时间',
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`id`),
+  KEY `project_name_index` (`project_name`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Table structure for project_polemic
--- ----------------------------
-DROP TABLE IF EXISTS `project_polemic`;
+
+
+# Dump of table project_polemic
+# ------------------------------------------------------------
+
 CREATE TABLE `project_polemic` (
   `id` bigint(20) NOT NULL,
   `project_group_id` bigint(20) DEFAULT NULL COMMENT '项目组id',
@@ -139,10 +151,11 @@ CREATE TABLE `project_polemic` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Table structure for receive_notice
--- ----------------------------
-DROP TABLE IF EXISTS `receive_notice`;
+
+
+# Dump of table receive_notice
+# ------------------------------------------------------------
+
 CREATE TABLE `receive_notice` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `notice_id` bigint(20) DEFAULT NULL COMMENT '通知id',
@@ -151,33 +164,36 @@ CREATE TABLE `receive_notice` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Table structure for role
--- ----------------------------
-DROP TABLE IF EXISTS `role`;
+
+
+# Dump of table role
+# ------------------------------------------------------------
+
 CREATE TABLE `role` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `create_time` datetime DEFAULT NULL COMMENT '创建时间',
   `name` varchar(255) DEFAULT NULL COMMENT '角色名称',
   `update_time` datetime DEFAULT NULL COMMENT '更新时间',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Table structure for role_acl
--- ----------------------------
-DROP TABLE IF EXISTS `role_acl`;
+
+
+# Dump of table role_acl
+# ------------------------------------------------------------
+
 CREATE TABLE `role_acl` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `role_id` bigint(20) DEFAULT NULL,
   `acl_id` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Table structure for score
--- ----------------------------
-DROP TABLE IF EXISTS `score`;
+
+
+# Dump of table score
+# ------------------------------------------------------------
+
 CREATE TABLE `score` (
   `id` bigint(20) NOT NULL,
   `user_id` bigint(20) DEFAULT NULL,
@@ -187,10 +203,11 @@ CREATE TABLE `score` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Table structure for user
--- ----------------------------
-DROP TABLE IF EXISTS `user`;
+
+
+# Dump of table user
+# ------------------------------------------------------------
+
 CREATE TABLE `user` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `code` varchar(255) DEFAULT NULL COMMENT '学号或工号',
@@ -210,12 +227,13 @@ CREATE TABLE `user` (
   `class_num` int(11) DEFAULT NULL COMMENT '班级',
   PRIMARY KEY (`id`),
   UNIQUE KEY `user_code` (`code`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Table structure for user_project_group
--- ----------------------------
-DROP TABLE IF EXISTS `user_project_group`;
+
+
+# Dump of table user_project_group
+# ------------------------------------------------------------
+
 CREATE TABLE `user_project_group` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `project_group_id` bigint(20) DEFAULT NULL,
@@ -230,13 +248,24 @@ CREATE TABLE `user_project_group` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Table structure for user_role
--- ----------------------------
-DROP TABLE IF EXISTS `user_role`;
+
+
+# Dump of table user_role
+# ------------------------------------------------------------
+
 CREATE TABLE `user_role` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `user_id` bigint(20) DEFAULT NULL,
   `role_id` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
