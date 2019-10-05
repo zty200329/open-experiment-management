bb90dbc839aa27ad4382ca6c77d354fb20e6cef5
panghu <792559738@qq.com>
2019-10-05 12:33:47 +0800

    完成项目驳回

--- src/main/java/com/swpu/uchain/openexperiment/enums/ProjectStatus.java
+++ src/main/java/com/swpu/uchain/openexperiment/enums/ProjectStatus.java
@@ -5,49 +5,40 @@ import lombok.Getter;
 /**
  * @Author: clf
  * @Date: 19-1-17
- * @Description:
- * 项目进度状态
+ * @Description: 项目进度状态
  */
 @Getter
 public enum ProjectStatus {
 
     /**
-     * 驳回修改
+     *
      */
-    REJECT_MODIFY(-1),
-    /**
-     * 申报
-     */
-    DECLARE(0),
-    /**
-     * 实验室审核通过
-     */
-    LAB_ALLOWED(1),
-    /**
-     * 二级单位审核通过
-     */
-    SECONDARY_UNIT_ALLOWED(2),
-    /**
-     * 立项
-     */
-    ESTABLISH(3),
-    /**
-     * 已上报学院领导
-     */
-    REPORT_COLLEGE_LEADER(5),
-    /**
-     * 中期检查
-     */
-    MID_TERM_INSPECTION(6),
-    /**
-     * 结题项目
-     */
-    CONCLUDED(7),
+    REJECT_MODIFY(-1,"驳回修改"),
+
+
+    DECLARE(0,"申报"),
+
+    LAB_ALLOWED(1,"实验室审核通过,待上报"),
+
+    LAB_ALLOWED_AND_REPORTED(2,"实验室审核通过并已上报"),
+
+    SECONDARY_UNIT_ALLOWED(3,"二级单位审核通过,待上报"),
+
+    SECONDARY_UNIT_ALLOWED_AND_REPORTED(4,"二级单位审核通过并已上报"),
+
+    ESTABLISH(5,"职能部门审核通过,立项"),
+
+    MID_TERM_INSPECTION(6,"中期检查"),
+
+    CONCLUDED(7,"结题项目"),
     ;
 
     private Integer value;
 
-    ProjectStatus(Integer value) {
+    private String tips;
+
+    ProjectStatus(Integer value, String tips) {
         this.value = value;
+        this.tips = tips;
     }
 }
