823ceeed31930cc1d43eba13f1aac5a1f6ae44e7
panghu <792559738@qq.com>
2019-10-05 11:37:57 +0800

    完成项目驳回

--- src/main/java/com/swpu/uchain/openexperiment/controller/ProjectInvokeController.java
+++ src/main/java/com/swpu/uchain/openexperiment/controller/ProjectInvokeController.java
@@ -108,7 +108,7 @@ public class ProjectInvokeController {
     }
 
     @ApiOperation("根据项目名模糊查询项目")
-    @GetMapping(value = "/selectProject", name = "根据项目名模糊查询项目")
+    @GetMapping(value = "/selectProject", name = "根据项目名模糊查询项目--可使用")
     public Result selectProject(String name){
         if (StringUtils.isEmpty(name)){
             return Result.error(CodeMsg.PARAM_CANT_BE_NULL);
@@ -116,7 +116,7 @@ public class ProjectInvokeController {
         return Result.success(projectService.selectByProjectName(name));
     }
 
-    @ApiOperation(" 驳回项目立项申请（实验室，二级单位，职能部门通用接口）")
+    @ApiOperation(" 驳回项目立项申请（实验室，二级单位，职能部门通用接口）--可使用")
     @PostMapping(value = "/rejectProjectApply")
     public Result rejectProjectApply(@Valid @RequestBody List<ProjectCheckForm> formList){
         return projectService.rejectProjectApply(formList);
