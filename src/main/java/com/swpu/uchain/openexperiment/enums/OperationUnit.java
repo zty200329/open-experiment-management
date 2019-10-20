aa810dbe28fbb84a9a4580fdf3457dfd8ed5deb8
panghu <792559738@qq.com>
2019-10-20 18:12:49 +0800

    修正
    Signed-off-by: denggaohu <panghu792559738@gmail.com>

---
+++ src/main/java/com/swpu/uchain/openexperiment/enums/OperationUnit.java
@@ -0,0 +1,26 @@
+package com.swpu.uchain.openexperiment.enums;
+
+import lombok.Getter;
+
+/**
+ * @author dengg
+ */
+@Getter
+public enum OperationUnit {
+
+    /**
+     *
+     */
+    LAB_ADMINISTRATOR(4,"实验室主任"),
+    SECONDARY_UNIT(5,"二级单位(学院领导)"),
+    FUNCTIONAL_DEPARTMENT(6,"职能部门");
+
+    private Integer value;
+
+    private String tips;
+
+    OperationUnit(Integer value, String tips) {
+        this.value = value;
+        this.tips = tips;
+    }
+}
