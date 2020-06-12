6ca07ebb033497563f4e6ed030d9a7e318e93047
zty200329 <1412306160@qq.com>
2020-06-13 02:32:19 +0800

    613儿童节 成果登记

---
+++ src/main/java/com/swpu/uchain/openexperiment/form/project/DeleteIconicResultForm.java
@@ -0,0 +1,13 @@
+package com.swpu.uchain.openexperiment.form.project;
+
+import lombok.Data;
+
+/**
+ * @author zty
+ * @date 2020/6/2 下午5:57
+ * @description:
+ */
+@Data
+public class DeleteIconicResultForm {
+    private Long id;
+}
